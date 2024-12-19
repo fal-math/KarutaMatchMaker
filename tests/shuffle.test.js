ef8041d22f4f88f9e8540c5893f8a9b8a267c53e
fal-mathPC <takada.y.ak@m.titech.ac.jp>
2024-12-19 21:03:26 +0900

    adjust test case

--- tests/shuffle.test.js
+++ tests/shuffle.test.js
@@ -30,7 +30,6 @@ const basicPower2TestExpectedPairs = [
     { ID: 204, 姓: "高橋", 名: "三郎", 所属: "梅かるた会", 欠席: "FALSE" },
   ],
 ];
-const basicPower2TestExpectedWalkovers = []; // 2べきなので不戦勝なし
 
 // --------------------------------------
 // テスト: 基本的動作: 偶数人数、全員出席
@@ -56,10 +55,8 @@ const basicEvenTestExpectedPairs = [
     { ID: 203, 姓: "佐藤", 名: "次郎", 所属: "竹かるた会", 欠席: "FALSE" },
     { ID: 204, 姓: "高橋", 名: "三郎", 所属: "梅かるた会", 欠席: "FALSE" },
   ],
-];
-const basicEvenTestExpectedWalkovers = [
-  { ID: 205, 姓: "糸魚川", 名: "久美子", 所属: "椿かるた会", 欠席: "FALSE" },
-  { ID: 206, 姓: "井上", 名: "奈々未", 所属: "鈴蘭かるた会", 欠席: "FALSE" },
+  [{ ID: 205, 姓: "糸魚川", 名: "久美子", 所属: "椿かるた会", 欠席: "FALSE" }],
+  [{ ID: 206, 姓: "井上", 名: "奈々未", 所属: "鈴蘭かるた会", 欠席: "FALSE" }],
 ];
 
 // --------------------------------------
@@ -81,11 +78,9 @@ const basicOddExpectedPairs = [
     { ID: 201, 姓: "田中", 名: "太郎", 所属: "桜かるた会", 欠席: "FALSE" },
     { ID: 202, 姓: "鈴木", 名: "花子", 所属: "松かるた会", 欠席: "FALSE" },
   ],
-];
-const basicOddExpectedWalkovers = [
-  { ID: 203, 姓: "佐藤", 名: "次郎", 所属: "竹かるた会", 欠席: "FALSE" },
-  { ID: 204, 姓: "高橋", 名: "三郎", 所属: "梅かるた会", 欠席: "FALSE" },
-  { ID: 205, 姓: "高橋", 名: "一郎", 所属: "椿かるた会", 欠席: "FALSE" },
+  [{ ID: 203, 姓: "佐藤", 名: "次郎", 所属: "竹かるた会", 欠席: "FALSE" }],
+  [{ ID: 204, 姓: "高橋", 名: "三郎", 所属: "梅かるた会", 欠席: "FALSE" }],
+  [{ ID: 205, 姓: "高橋", 名: "一郎", 所属: "椿かるた会", 欠席: "FALSE" }],
 ];
 
 // --------------------------------------
@@ -102,7 +97,6 @@ const allAbsentTestInputData = [
 
 // 基本的動作正解ペア1
 const allAbsentTestExpectedPairs = [];
-const allAbsentTestExpectedWalkovers = [];
 
 // --------------------------------------
 // 同会対決回避: 4人、全員出席
@@ -127,7 +121,6 @@ const avoidSameClubExpectedPairs1 = [
     { ID: 204, 姓: "高橋", 名: "三郎", 所属: "竹かるた会", 欠席: "FALSE" },
   ],
 ];
-const avoidSameClubExpectedWalkovers1 = [];
 
 // --------------------------------------
 // 同会対決回避: 8人、全員出席
@@ -163,7 +156,6 @@ const avoidSameClubExpectedPairs2 = [
     { ID: 208, 姓: "稲荷", 名: "礼二", 所属: "桜かるた会", 欠席: "FALSE" },
     { ID: 206, 姓: "井上", 名: "奈々未", 所属: "竹かるた会", 欠席: "FALSE" },
   ],]
-const avoidSameClubExpectedWalkovers2 = [];
 
 
 // --------------------------------------
@@ -188,7 +180,6 @@ const avoidSameClubRareCaseExpectedPairs1 = [
     { ID: 203, 姓: "長野", 名: "健太", 所属: "桜かるた会", 欠席: "FALSE" },
     { ID: 201, 姓: "田中", 名: "太郎", 所属: "緑かるた会", 欠席: "FALSE" },
   ],]
-const avoidSameClubRareCaseExpectedWalkovers1 = [];
 
 // --------------------------------------
 // 同会回避&先頭チェック: 8人
@@ -224,8 +215,6 @@ const avoidSameClubRareCaseExpectedPairs2 = [
     { ID: 207, 姓: "糸魚川", 名: "久美子", 所属: "桜かるた会", 欠席: "FALSE" },
     { ID: 203, 姓: "長野", 名: "健太", 所属: "緑かるた会", 欠席: "FALSE" },
   ],]
-const avoidSameClubRareCaseExpectedWalkovers2 = [];
-
 
 // --------------------------------------
 // 同会回避できない: 4人
@@ -249,8 +238,6 @@ const sameClubExpectedPairs1 = [
     { ID: 203, 姓: "長野", 名: "健太", 所属: "桜かるた会", 欠席: "FALSE" },
     { ID: 204, 姓: "稲荷", 名: "礼二", 所属: "桜かるた会", 欠席: "FALSE" },
   ],]
-const sameClubExpectedWalkovers1 = [];
-
 
 // --------------------------------------
 // 同会回避できない: 4人
@@ -274,69 +261,57 @@ const sameClubExpectedPairs2 = [
     { ID: 202, 姓: "鈴木", 名: "花子", 所属: "桜かるた会", 欠席: "FALSE" },
     { ID: 204, 姓: "稲荷", 名: "礼二", 所属: "桜かるた会", 欠席: "FALSE" },
   ],]
-const sameClubExpectedWalkovers2 = [];
-
 
 const testCases = [
   {
     description: "基本的動作: 2べき人数、全員出席",
     input: basicPower2TestInputData,
     expectedPairs: basicPower2TestExpectedPairs,
-    expectedWalkovers: basicPower2TestExpectedWalkovers,
   },
   {
     description: "基本的動作: 偶数人数、全員出席",
     input: basicEvenTestInputData,
     expectedPairs: basicEvenTestExpectedPairs,
-    expectedWalkovers: basicEvenTestExpectedWalkovers,
   },
   {
     description: "基本的動作: 奇数人数、全員出席",
     input: basicOddTestInputData,
     expectedPairs: basicOddExpectedPairs,
-    expectedWalkovers: basicOddExpectedWalkovers,
   },
   {
     description: "特殊ケース: 全員欠席",
     input: allAbsentTestInputData,
     expectedPairs: allAbsentTestExpectedPairs,
-    expectedWalkovers: allAbsentTestExpectedWalkovers,
   },
   {
     description: "同会対決回避: 4人、全員出席",
     input: avoidSameClubTestInputData1,
     expectedPairs: avoidSameClubExpectedPairs1,
-    expectedWalkovers: avoidSameClubExpectedWalkovers1,
   },
   {
     description: "同会対決回避: 8人、全員出席",
     input: avoidSameClubTestInputData2,
     expectedPairs: avoidSameClubExpectedPairs2,
-    expectedWalkovers: avoidSameClubExpectedWalkovers2,
   },
   {
     description: "同会回避&先頭チェック: 4人",
     input: avoidSameClubRareCaseTestInputData1,
     expectedPairs: avoidSameClubRareCaseExpectedPairs1,
-    expectedWalkovers: avoidSameClubRareCaseExpectedWalkovers1,
   },
   {
     description: "同会回避&先頭チェック: 8人",
     input: avoidSameClubRareCaseTestInputData2,
     expectedPairs: avoidSameClubRareCaseExpectedPairs2,
-    expectedWalkovers: avoidSameClubRareCaseExpectedWalkovers2,
   },
   {
     description: "同会回避できない: 4人",
     input: sameClubTestInputData1,
     expectedPairs: sameClubExpectedPairs1,
-    expectedWalkovers: sameClubExpectedWalkovers1,
   },
   {
     description: "同会回避できない: 4人",
     input: sameClubTestInputData2,
     expectedPairs: sameClubExpectedPairs2,
-    expectedWalkovers: sameClubExpectedWalkovers2,
   },
 ];
 
@@ -347,14 +322,11 @@ const testCases = [
 describe("generatePairs - Data Driven Tests", () => {
   test.each(testCases)(
     "$description",
-    ({ input, expectedPairs, expectedWalkovers }) => {
-      const { pairs, walkovers } = generatePairs(input, mockShuffle);
+    ({ input, expectedPairs }) => {
+      const pairs = generatePairs(input, mockShuffle);
 
       // ペアリングの結果が正しいか
       expect(pairs).toEqual(expectedPairs);
-
-      // 不戦勝の結果が正しいか
-      expect(walkovers).toEqual(expectedWalkovers);
     }
   );
 });
